11d71cd0ed47e8812ce42ad7ee65f511651fafc4
peter <peterzhou43@gmail.com>
2019-01-21 18:36:19 -0800

    1

--- run.sh
+++ run.sh
@@ -1,21 +1,25 @@
 sudo apt-get update
 sudo apt-get install git-core
 
-# download the SWNET repo
-
-sudo bash setup.sh
-
 # install cmake
 # refrecen https://geeksww.com/tutorials/operating_systems/linux/installation/downloading_compiling_and_installing_cmake_on_linux.php
-wget http://www.cmake.org/files/v2.8/cmake-2.8.3.tar.gz
-tar xzf cmake-2.8.3.tar.gz
-cd cmake-2.8.3
-# In order to configure cmake before installation, run command below.
-./configure --prefix=/opt/cmake
-# We basically instructed the install script to install CMake in /opt/cmake.
+wget http://www.cmake.org/files/v3.10/cmake-3.10.3.tar.gz
+tar xzf cmake-3.10.3.tar.gz
+cd cmake-3.10.3
+./configure
 # Compilation:
 make
 # Installation:
-make install
+sudo make install
 # Verification:
-/opt/cmake/bin/cmake -version
+cmake -version
+
+# download repo:111
+cd ~/Documents/
+mkdir repo
+
+wget --user <username> --password <password> https://bitbucket.org/socketweaver/swnet/get/9f67b878ca56.zip
+unzip 9f67b878ca56.zip
+cd socketweaver-swnet-9f67b878ca56
+make new-developer
+make
